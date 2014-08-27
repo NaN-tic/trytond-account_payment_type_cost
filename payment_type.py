37b02900faba18891a6e42bf98905931ec69ccd0
Guillem Barba <guillem@nan-tic.com>
2014-08-27 12:23:23 +0200

    Removed cost_percent constraint to allow payment types with discounts as cost

--- payment_type.py
+++ payment_type.py
@@ -1,8 +1,10 @@
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
+from trytond import backend
 from trytond.model import fields
 from trytond.pool import PoolMeta
 from trytond.pyson import Eval, Not
+from trytond.transaction import Transaction
 
 __all__ = ['PaymentType']
 __metaclass__ = PoolMeta
@@ -23,9 +25,10 @@ class PaymentType:
             }, depends=['has_cost'])
 
     @classmethod
-    def __setup__(cls):
-        super(PaymentType, cls).__setup__()
-        cls._sql_constraints += [
-            ('cost_percent', 'CHECK(COALESCE(cost_percent,0.0) >= 0.0)',
-                'Cost percent must be greater than 0'),
-            ]
+    def __register__(cls, module_name):
+        TableHandler = backend.get('TableHandler')
+        cursor = Transaction().cursor
+
+        # Migration from 3.2.0: removed constraint
+        table = TableHandler(cursor, cls, module_name)
+        table.drop_constraint('cost_percent')
