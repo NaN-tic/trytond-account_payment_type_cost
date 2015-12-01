eb1eb6a22190554f6f51adbcda0bfe189f46dd30
resteve <resteve@zikzakmedia.com>
2015-12-01 11:01:55 +0100

    Tests

--- tests/test_account_payment_type_cost.py
+++ tests/test_account_payment_type_cost.py
@@ -1,32 +1,18 @@
-#!/usr/bin/env python
+# This file is part of the account_payment_type_cost module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 import unittest
-import doctest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
-from trytond.tests.test_tryton import doctest_setup, doctest_teardown
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class TestCase(unittest.TestCase):
-    'Test module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('account_payment_type_cost')
-
-    def test0005views(self):
-        'Test views'
-        test_view('account_payment_type_cost')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+class AccountPaymentTypeCostTestCase(ModuleTestCase):
+    'Test Account Payment Type Cost module'
+    module = 'account_payment_type_cost'
 
 
 def suite():
     suite = trytond.tests.test_tryton.suite()
-    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(TestCase))
-    suite.addTests(doctest.DocFileSuite('scenario_invoice.rst',
-            setUp=doctest_setup, tearDown=doctest_teardown, encoding='utf-8',
-            optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
+    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
+        AccountPaymentTypeCostTestCase))
     return suite
