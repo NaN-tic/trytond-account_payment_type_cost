c395349831e42ca27270a406530af3bb1ada460c
Juanjo Garcia <juanjo.garcia@nan-tic.com>
2024-01-11 14:05:50 +0100

    Send only the required fields when fetching a readonly view definition. issue11134. Task #050808

--- payment_type.py
+++ payment_type.py
@@ -27,12 +27,12 @@ class PaymentType(metaclass=PoolMeta):
     cost_percent = fields.Numeric('Cost (%)', digits=(8, 4), states={
             'required': Eval('has_cost', False),
             'invisible': Not(Eval('has_cost', False)),
-            }, depends=['has_cost'])
+            })
     compute_over_total_amount = fields.Boolean('Compute over total amount',
         help='Check it if you want to compute cost over total amount with tax,'
         ' otherwise will compute it over total amount without tax', states={
             'invisible': Not(Eval('has_cost', False)),
-            }, depends=['has_cost'])
+            })
 
     @classmethod
     def __register__(cls, module_name):
